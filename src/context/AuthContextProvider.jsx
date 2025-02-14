726c50b8f0b5db3bf1db976c127ee6b55be945bb
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-14 07:23:40 +0600

    added login signup email gmail functions

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from 'react';
 import { AuthContext } from './Authcontext';
 import PropTypes from 'prop-types';
-import { onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import auth from '../utils/firebase/__config__';
 
 const AuthContextProvider = ({ children }) => {
     let [user, setUser] = useState(null);
     let [loading, setLoading] = useState(true);
 
+    // login with google 
+    const provider = new GoogleAuthProvider()
+    const loginWithGoogle = () => {
+        return signInWithPopup(auth, provider)
+    }
+    // creat a account with email and password 
+    const signUp = (email, pass) => {
+        return createUserWithEmailAndPassword(auth, email, pass)
+    }
+    // login with email and password 
+    const login = (email, pass) => {
+        return signInWithEmailAndPassword(auth, email, pass)
+    }
     // observer here to check if user is logged in or not
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
@@ -17,11 +30,7 @@ const AuthContextProvider = ({ children }) => {
         return () => unsubscribe()
     }, [])
 
-    const data = {
-        user,
-        loading,
-
-    }
+    const data = { user, loading, signUp, login, loginWithGoogle,  }
     return (
         <AuthContext.Provider value={data}>
             {children}
