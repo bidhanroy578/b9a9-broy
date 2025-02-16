2d94d4110dec13590422a61ce98dec754b479ebc
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-16 09:06:48 +0600

    choose slider made responsive

--- src/components/home/ChooseSlider.jsx
+++ src/components/home/ChooseSlider.jsx
@@ -8,18 +8,18 @@ const Chooseslider = () => {
             .then(data => setChoose(data))
     }, [])
     return (
-        <div className='my-10'>
-            <h2 className='text-3xl font-semibold text-center leading-20'>Experience Luxury Like Never Before</h2>
+        <div>
+            <h2 className='text-3xl font-semibold text-center my-10'>Experience Luxury Like Never Before</h2>
             <div className="flex justify-center">
-                <div className="p-3 carousel carousel-vertical rounded-box w-[80vw] h-[60vh] shadow-2xl">
+                <div className=" carousel carousel-vertical rounded-box w-[80vw] h-[70vh] shadow-2xl">
                     {
                         choose.map(i =>
-                            <div key={i.id} className="my-2 grid grid-cols-2 gap-4 carousel-item h-full w-full">
-                                <div className="h-full text-center content-center w-full bg-slate-200 rounded-2xl overflow-hidden">
+                            <div className="my-2 grid grid-rows-2 md:grid-cols-2 gap-4 carousel-item h-full w-full" key={i.id}>
+                                <div className="h-full border border-red-400 text-center content-center w-full bg-slate-200 rounded-2xl overflow-hidden">
                                     <div style={{ backgroundImage: `url(${i.image})` }} className="bg-cover bg-center h-full w-full"></div>
                                 </div>
-                                <div className="text-center content-center p-5">
-                                    <h3 className="font-bold text-3xl leading-20">{i.title}</h3>
+                                <div className="text-center content-center px-5">
+                                    <h3 className="font-bold text-3xl mb-4">{i.title}</h3>
                                     <p className="text-lg">{i.description}</p>
                                 </div>
                             </div>
