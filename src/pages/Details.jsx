f8c4dccee033f154f66530564de8d875d7750b38
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-16 10:42:46 +0600

    made details page responsive

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -13,7 +13,7 @@ const Details = () => {
 
     return (
         <div className="space-y-8">
-            <div className="h-screen grid grid-cols-2 gap-2 relative">
+            <div className="min-h-screen grid md:grid-cols-2 gap-2 relative">
                 <img src={image} alt="" className="object-cover h-full w-full" />
                 <div className="grid grid-cols-2 gap-2">
                     <img src={image} className="object-cover h-full w-full" />
@@ -21,17 +21,17 @@ const Details = () => {
                     <img src={image} className="object-cover h-full w-full" />
                     <img src={image} className="object-cover h-full w-full" />
                 </div>
-                <div className="absolute bottom-[10%] left-[10%] z-10 text-xl">
+                <div className="md:absolute bottom-[10%] left-[10%] z-10 text-xl">
                     <div className="flex gap-2">
                         <button className="btn btn-lg rounded-full"><GrFavorite /> Save</button>
                         <button className='btn btn-lg rounded-full'><IoMdShare /> Share</button>
                     </div>
                 </div>
-                <button className="absolute bottom-[10%] right-[10%] z-10 btn btn-lg rounded-full ">See More </button>
+                <button className="md:absolute bottom-[10%] right-[10%] z-10 btn btn-lg rounded-full ">See More </button>
             </div>
 
-            <div className="grid grid-cols-3">
-                <div className="col-span-2">
+            <div className="md:grid grid-cols-3">
+                <div className="col-span-2 p-2">
                     <div>
                         <h3 className="text-2xl leading-5 font-medium">{title}</h3>
                         <h4 className="text-3xl leading-20 opacity-85 font-light">{price}</h4>
