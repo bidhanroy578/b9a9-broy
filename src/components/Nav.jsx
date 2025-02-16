d76efe80522fbe58b2ca33c6e584d30aa02da13a
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-16 14:22:57 +0600

    navbar visibility issue fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -41,7 +41,7 @@ const Nav = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content text-black bg-white rounded-box z-1 mt-3 w-52 p-2 shadow">
                             {mainLinks}
                         </ul>
                     </div>
